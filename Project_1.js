5d2445c69cc8b1f7360b67f22faf3d338254996e
shizuka-virus <muhammadfarooq1085@gmail.com>
2024-08-01 22:26:34 +0500

    done

--- Project_1.js
+++ Project_1.js
@@ -1,1 +1,17 @@
-
+const button=document.querySelectorAll(".button");
+button.forEach((e)=>{
+  e.addEventListener('click',function(ev){
+    if(ev.target.id==="blue"){
+      e.parentElement.parentElement.style.backgroundColor=`${ev.target.id}`;
+    }
+    if(ev.target.id==="grey"){
+      e.parentElement.parentElement.style.backgroundColor=`${ev.target.id}`;
+    }
+    if(ev.target.id==="yellow"){
+      e.parentElement.parentElement.style.backgroundColor=`${ev.target.id}`;
+    }
+    if(ev.target.id==="white"){
+      e.parentElement.parentElement.style.backgroundColor=`${ev.target.id}`;
+    }
+  })
+})
