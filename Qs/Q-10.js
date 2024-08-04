30d77e27a4332a871f61a8c61607a7a8e8db6f88
shizuka-virus <muhammadfarooq1085@gmail.com>
2024-08-04 22:19:23 +0500

    10

---
+++ Qs/Q-10.js
@@ -0,0 +1,8 @@
+// JavaScript Program to Replace all Instances of a Character in a String
+// program to replace all instances of a character in a string
+
+const string = 'Learning JavaScript Program';
+
+const result = string.replace(/a/g, "A");
+
+console.log(result);
