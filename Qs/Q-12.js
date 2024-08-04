2eb188bcb926783f62031038e045cb7c7b2463ca
shizuka-virus <muhammadfarooq1085@gmail.com>
2024-08-04 22:19:42 +0500

    12

---
+++ Qs/Q-12.js
@@ -0,0 +1,18 @@
+// JavaScript Program to Merge Property of Two Objects
+// program to merge property of two objects
+
+// object 1
+const person = {
+    name: 'Jack',
+    age:26
+}
+
+// object 2
+const student = {
+    gender: 'male'
+}
+
+// merge two objects
+const newObj = Object.assign(person, student);
+
+console.log(newObj);
