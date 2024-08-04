10a1d43aa0d1610fcd5b592b05b94d72101b72c2
shizuka-virus <muhammadfarooq1085@gmail.com>
2024-08-04 22:19:52 +0500

    14

---
+++ Qs/Q-14.js
@@ -0,0 +1,21 @@
+// JavaScript Program to Check if An Object is An Array
+// program to check if an object is an array
+
+function checkObject(arr) {
+
+    // check if arr is array
+    const result = Array.isArray(arr);
+
+    if(result) {
+        console.log(`[${arr}] is an array.`);
+    }
+    else {
+        console.log(`${arr} is not an array.`);
+    }
+
+}
+
+const array = [1, 2, 3];
+
+// call the function
+checkObject(array);
